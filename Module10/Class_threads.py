a1a599c64cc5e778c01416a78535f914bcda3196
LifeInputter <alphacube36@gmail.com>
2024-06-17 20:15:45 +0300

    ДЗ к уроку "Class Threads". Ура!!!

--- Module10/Class_threads.py
+++ Module10/Class_threads.py
@@ -1,7 +1,15 @@
+# Напишите программу с использованием механизмов многопоточности, которая создает два потока-рыцаря.
+# Каждый рыцарь должен иметь имя (name) и умение(skill). Умение рыцаря определяет, сколько времени потребуется рыцарю, чтобы выполнить свою защитную миссию для королевства.
+# Враги будут нападать в количестве 100 человек. Каждый день рыцарь может ослабить вражеское войско на skill-человек.
+# Если у рыцаря skill равен 20, то защищать крепость он будет 5 дней (5 секунд в программе).
+# Чем выше умение, тем быстрее рыцарь защитит королевство.
 import time
 from threading import Thread
+from termcolor import cprint
 from collections import defaultdict
-from time import sleep, time
+from time import sleep
+
+
 class Knight(Thread):
     def __init__(self, name, skill, *args, **kwargs):
         super(Knight, self).__init__(*args, **kwargs)
@@ -9,47 +17,34 @@ class Knight(Thread):
         self.skill = skill
 
     def run(self):
-
         enemy = 100
         enemy_remains = 100
         counter = 0
         if enemy >= 0:
             for i in range(20):
                 counter += 1
+                time.sleep(1)
                 if enemy_remains == 0:
                     break
-                enemy_remains = enemy - self.skill*counter
+                enemy_remains = enemy - self.skill * counter
 
-                print(f'{self.name} сражается {counter} день, убито {self.skill} воинов, осталось {enemy_remains} врагов')
+                print(
+                    f'{self.name} сражается {counter} день, убито {self.skill} воинов, осталось {enemy_remains} врагов',
+                    flush=True)
         if enemy_remains == 0:
-            print(f'{self.name} одержал победу  спустя {counter - 1} дней!')
-
-
-        # time.sleep(1)
-        # if time.sleep(1):
-        #     print("")
-        # enemy_killed = defaultdict(int)
-        # for person in range(enemy):
-        #     print(f'{self.name} сражается, убито {self.skill} воинов, осталось {enemy - self.skill} врагов')
-        # if enemy_killed == enemy:
-        #     print("Все враги уничтожены, битва закончилась")
+            cprint(f'{self.name} одержал победу  спустя {counter - 1} дней!', color='cyan')
 
 
 knight1 = Knight("Sir Lancelot", 10)
 knight2 = Knight("Sir Galahad", 20)
 
-print(f'{knight1.name}: святые угодники, на нас напали!')
-print(f'{knight2.name}: OMG, на нас напали!')
+cprint(f'{knight1.name}: святые угодники, на нас напали!', color="yellow")
+cprint(f'{knight2.name}: OMG, на нас напали!', color="yellow")
 
 knight1.start()
 knight2.start()
 
-
-
 knight1.join()
 knight2.join()
 
-print("Все битвы закончились")
-
-
-
+print("-"*10,"Все битвы закончились","-"*10)
