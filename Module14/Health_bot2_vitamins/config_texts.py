ad287107e3039dd783e35ee9e3b91e21b2c5db0e
LifeInputter <alphacube36@gmail.com>
2024-07-30 00:01:31 +0300

    Практическая работа "Создание бота". Модуль 13,14

---
+++ Module14/Health_bot2_vitamins/config_texts.py
@@ -0,0 +1,15 @@
+API = ""
+
+vitamin_C = 3250
+vitamin_B = 7000
+vitamin_A = 3000
+vitamin_E = 2500
+
+
+product1 = f'Название: Product1 | Описание: витамин C | Цена: {vitamin_C}'
+product2 = f'Название: Product2 | Описание: витамин B | Цена: {vitamin_B}'
+product3 = f'Название: Product3 | Описание: витамин A | Цена: {vitamin_A}'
+product4 = f'Название: Product4 | Описание: витамин E | Цена: {vitamin_E}'
+
+
+confirm = "Вы успешно приобрели продукт!"
---
+++ Module14/Health_bot2_vitamins/kb.py
@@ -0,0 +1,25 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+start_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Информация"),
+            KeyboardButton(text="Рассчитать"),
+            KeyboardButton(text="Купить")
+
+        ]
+    ], resize_keyboard=True
+)
+
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+
+        [InlineKeyboardButton(text='Продукт1', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Продукт2', callback_data="product_buying")],
+        [InlineKeyboardButton(text="Продукт3", callback_data='product_buying')],
+        [InlineKeyboardButton(text='Продукт4', callback_data='product_buying')]
+
+    ]
+)
+
