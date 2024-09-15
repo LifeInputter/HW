d2cd592f1baa4c472c4ed5b871437b7b931f60f3
LifeInputter <alphacube36@gmail.com>
2024-09-16 00:19:02 +0300

    Тема "Модели баз данных в Django". Задача "Модели игрового магазина". 19.1

--- Module19/DjangoGameShop/task1/models.py
+++ Module19/DjangoGameShop/task1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import timezone
 
+
 # Create your models here.
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
@@ -10,14 +11,14 @@ class Buyer(models.Model):
     def __str__(self):
         return self.name
 
+
 class Game(models.Model):
     title = models.CharField(max_length=250)  # название игры
-    cost = models.DecimalField(max_digits=10, decimal_places=2)   #цена(DecimalField)
-    size = models.DecimalField(max_digits=10, decimal_places=2)   #описание(неограниченное кол-во текста)
-    description = models.TextField(db_default=True)    # описание(неограниченное кол-во текста)
-    age_limited = models.BooleanField(default=False) #ограничение возраста 18+
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # цена(DecimalField)
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # описание(неограниченное кол-во текста)
+    description = models.TextField(db_default=True)  # описание(неограниченное кол-во текста)
+    age_limited = models.BooleanField(default=False)  # ограничение возраста 18+
     buyer = models.ManyToManyField(Buyer, related_name="games")
 
     def __str__(self):
         return self.title
-
